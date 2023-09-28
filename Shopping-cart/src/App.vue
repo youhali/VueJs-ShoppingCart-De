cbed28eebb47ad2b7686e44ed12e3e162d787007
youhali <131718477+youhali@users.noreply.github.com>
2023-09-28 15:27:20 +0000

    🛒Shoppint cart Entrega de listas

--- Shopping-cart/src/App.vue
+++ Shopping-cart/src/App.vue
@@ -1,11 +1,21 @@
 <script setup>
 import { ref } from 'vue';
 const header= ref('Mi carrito de compras')
+const items = ref ([
+  {id: 1, label: '10 bolillos'},
+  {id: 2, label: '1 lata de frijoles'},
+  {id: 3, label: '2 lata de atún'}
+])
 
 </script>
 
 <template>
-  <h1><i class="material-icons shopping-cart-icon">local_mall</i> {{ header }}</h1>
+ <h1>{{ header }}</h1>
+ <ul>
+  <li v-for="({ id, label }, index) in items" v-bind:key="id">
+    🔹 - {{ index }} - {{ label }}
+  </li>
+</ul>
 </template> 
 
 <style scoped>
--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <script setup>
 import { ref } from 'vue';
 const header= ref('Mi carrito de compras')
-
+const items = ref({
+  "item1": { id: 1, label: '10 bolillos' },
+  "item2": { id: 2, label: '1 lata de frijoles' },
+  "item3": { id: 3, label: '2 lata de atún' },
+});
 </script>
 
 <template>
-  <h1><i class="material-icons shopping-cart-icon">local_mall</i> {{ header }}</h1>
-</template> 
+  <h1>{{ header }}</h1>
+  <ul>
+    <li v-for="({ id, label }, index) in items" v-bind:key="id">
+			♥ -{{ index }}- {{ label }}
+		</li>
+  </ul>
+</template>
 
 <style scoped>
 .shopping-cart-icont{
