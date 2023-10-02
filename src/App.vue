ca46a1f3155ca12e1e3a07b394f9656417ff2a04
youhali <131718477+youhali@users.noreply.github.com>
2023-10-02 18:52:54 +0000

    🐋Feature: Enlazado con checkboxes

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,21 @@ const items = ref({
   "item2": { id: 2, label: '1 lata de frijoles' },
   "item3": { id: 3, label: '2 lata de atún' },
 });
+const newItem= ref('1 Bolsa 🛍️💰');
+const newItemHighPriority = ref("false");
 </script>
 
 <template>
-  <h1>{{ header }}</h1>
-  <ul>
+    <h1> <i class="material-icons shopping-cart-icon">local_mall</i> {{ header }}</h1>
+<h1></h1>
+<input v-model.trim="newItem" type="text" placeholder="Ingresar articulo">
+<!-- Chek Boxes -->
+<label>
+  <input v-model="newItemHighPriority" type="checkbox">
+  Altaprioridad
+</label>
+{{ newItemHighPriority === "high" ?"🔥" :"🧊"}}
+<ul>
     <li v-for="({ id, label }, index) in items" v-bind:key="id">
 			♥ -{{ index }}- {{ label }}
 		</li>
